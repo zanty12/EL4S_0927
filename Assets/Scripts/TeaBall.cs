b33b4efb1cddfba179e0b54fe7c4cb587491a83b
Tokichiro_Nishikubo <nishikubo.tokichiro@gmail.com>
2025-09-27 14:33:10 +0900

    change titlescene

--- Assets/Scripts/TeaBall.cs
+++ Assets/Scripts/TeaBall.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class TeaBall : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer.color = ColorPicker.color;
     }
 
     // Update is called once per frame
